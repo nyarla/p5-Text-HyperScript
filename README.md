7f90c5e788338f3554e3ed5dfbdc38d62e9b6d59
nyarla <nyarla@kalaclista.com>
2022-09-29 08:36:26 +0900

    feat: import Minilla boilerplate

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# NAME
+
+Text::HyperScript - It's new $module
+
+# SYNOPSIS
+
+    use Text::HyperScript;
+
+# DESCRIPTION
+
+Text::HyperScript is ...
+
+# LICENSE
+
+Copyright (C) nyarla.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+nyarla <nyarla@kalaclista.com>
---
+++ lib/Text/HyperScript.pm
@@ -0,0 +1,39 @@
+package Text::HyperScript;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Text::HyperScript - It's new $module
+
+=head1 SYNOPSIS
+
+    use Text::HyperScript;
+
+=head1 DESCRIPTION
+
+Text::HyperScript is ...
+
+=head1 LICENSE
+
+Copyright (C) nyarla.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+nyarla E<lt>nyarla@kalaclista.comE<gt>
+
+=cut
+
